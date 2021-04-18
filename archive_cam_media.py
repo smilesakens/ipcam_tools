0b6651ede22e81b5e7d74ad84db7367fbfabea33
smiles-_- <nakens@gmail.com>
2021-04-18 04:05:28 -0400

    fixed bug in for loop

--- archive_cam_media.py
+++ archive_cam_media.py
@@ -14,7 +14,9 @@ if __name__ == '__main__':
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', dest='config_path', default='cam.config', help='Config containing IP address, username and password')
+    parser.add_argument('--host', dest='host', required='true', help='Camera hostname or IP address')
+    parser.add_argument('-u', '--username', dest='username', required='true', help='Camera username')
+    parser.add_argument('-p', '--password', dest='password', required='true', help='Camera password')
     parser.add_argument('-d', '--delta', dest='delta', default='5', type=int, help='Number of minutes to search before/after target_time')
     parser.add_argument('-t', '--target_time', dest='target_time', help='Target timestamp to search before/after (ISO Format)')
     parser.add_argument('--full_caps', dest='full_cap_path', required='true', help='Directory containing full capture files')
@@ -22,9 +24,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    ipaddr, username, password = cam_query.get_cam_config(args.config_path)
-
-    ipcam = dahua_rpc.DahuaRpc(host=ipaddr, username=username, password=password)
+    ipcam = dahua_rpc.DahuaRpc(host=args.host, username=args.username, password=args.password)
     ipcam.login()
 
     if not args.target_time:
@@ -39,20 +39,24 @@ if __name__ == '__main__':
 
     results = []
     for media_metadata_chunk, token in cam_query.get_media_metadata(ipcam, cam_query.PARAMS):
-        pprint.pprint(media_metadata_chunk)
         if media_metadata_chunk.get('result') == True:
+            if not media_metadata_chunk['params'].get('infos'):
+                # If the timerange had no events, infos will be None
+                continue
             for media_item in media_metadata_chunk['params'].get('infos'):
                 results.append(media_item)
     resp = ipcam.request('mediaFileFind.destroy', object_id=token)
 
-    for media_file in results:
-        output_fpath, media_bytez = cam_query.save_media_files(ipcam, media_file, args.output_path)
-        cam_query.copy_file_obj(output_fpath, media_bytez)
+    if not results == []:
+        for media_file in results:
+            output_fpath, media_bytez = cam_query.save_media_files(ipcam, media_file, args.output_path)
+            cam_query.copy_file_obj(output_fpath, media_bytez)
 
     resp = ipcam.request('global.logout')
 
-    for full_cap in [start_time.strftime('%Y-%m-%d_%H%M%S-full.mp4'), end_time.strftime('%Y-%m-%d_%H%M%S-full.mp4')]:
-        full_cap_path = os.path.join(args.full_cap_path, full_cap)
-        shutil.copy(full_cap_path, args.output_path)
+    if not results == []:
+        for full_cap in [start_time.strftime('%Y-%m-%d_%H%M%S-full.mp4'), end_time.strftime('%Y-%m-%d_%H%M%S-full.mp4')]:
+            full_cap_path = os.path.join(args.full_cap_path, full_cap)
+            shutil.copy(full_cap_path, args.output_path)
 
 
